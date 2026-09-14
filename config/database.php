108e06adbfc34520283861aaf45d985598d3f34e
sky-dev <hoangkien0705@users.noreply.github.com>
2026-09-14 22:18:00 +0700

    Fix SQLite DB_PREFIX configuration and array to string conversion handling

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+            'prefix' => env('DB_PREFIX', 'tasty'),
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
